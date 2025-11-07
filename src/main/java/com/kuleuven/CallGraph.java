0923fde9ae87f56e78228f79c35961ff2808a10c
SonnyZ789123 <yoran.mertens@telenet.be>
2025-11-07 21:41:10 +0100

    Save call graph to file

--- src/main/java/com/kuleuven/CallGraph.java
+++ src/main/java/com/kuleuven/CallGraph.java
@@ -12,6 +12,8 @@ import sootup.java.core.JavaSootMethod;
 import sootup.java.core.types.JavaClassType;
 import sootup.java.core.views.JavaView;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Optional;
 
@@ -42,14 +44,16 @@ public class CallGraph {
 
         JavaSootMethod sootMethod = opt.get();
 
-//        CallGraphAlgorithm cha = new ClassHierarchyAnalysisAlgorithm(view);
-        CallGraphAlgorithm rta = new RapidTypeAnalysisAlgorithm(view); // or RTA
+        CallGraphAlgorithm cha = new ClassHierarchyAnalysisAlgorithm(view);
+//        CallGraphAlgorithm rta = new RapidTypeAnalysisAlgorithm(view); // or RTA
 
-        sootup.callgraph.CallGraph cg = rta.initialize(Collections.singletonList(methodSignature));
+        sootup.callgraph.CallGraph cg = cha.initialize(Collections.singletonList(methodSignature));
 
-        cg.callsFrom(methodSignature).stream()
-                .forEach(tgt -> System.out.println(methodSignature + " may call " + tgt));
-
-//        System.out.println("dotUrl: " + cg.exportAsDot());
+        try (FileWriter writer = new FileWriter("graph_raw.dot")) {
+            writer.write(cg.exportAsDot());
+            System.out.println("DOT file written to graph_raw.dot");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
